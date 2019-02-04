281b4969b8f665dab1a42a5f154004b80cf87f6a
Jimmy Svensson <jimmy888swe@gmail.com>
2019-02-04 10:05:24 +0100

    nodeExpress

---
+++ express.js
@@ -0,0 +1,28 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+// http://localhost:3000/
+app.get('/', (req,res)=>{
+    res.send('hej');
+});
+// http://localhost:3000/about.html
+app.get('/about', (req, res)=>{
+    res.sendFile(__dirname + '/about.html');
+});
+// http://localhost:3000/  + dynamsik adress
+app.get('/products/:productID', (req, res) => {
+    res.send('Här kommer info om produkt: ' + req.params.productID);
+});
+
+// http://localhost:3000/products?category=2345 query
+app.get('/products', (req, res) => {
+    res.send('Här kommer info från queryn: ' + req.query.category)
+});
+// http://localhost:3000/ + felstavning.
+app.get('*', (req, res) => {
+    res.send('404');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server lyssnar på port: ${port}`)
+});
